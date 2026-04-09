0b54315daf6d400cdf41e20e66d2bb4b2a2b49d8
gabrielsuj <gm449870@gmail.com>
2026-04-09 11:25:41 -0300

    feat: registra rotas de startups

--- backend/src/modules/startups/startups.routes.ts
+++ backend/src/modules/startups/startups.routes.ts
@@ -5,6 +5,6 @@ const router = Router();
 const controller = new StartupsController();
 
 router.get('/', (req, res) => controller.listar(req, res));
-router.get('/id', (req, res) => controller.buscarPorId(req, res));
+router.get('/:id', (req, res) => controller.buscarPorId(req, res));
 
 export default router;
