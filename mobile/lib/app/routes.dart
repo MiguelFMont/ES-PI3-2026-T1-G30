8cfb4fbe64b61b846524a5d8e99377644db0f3f7
gabrielsuj <gm449870@gmail.com>
2026-04-22 14:20:20 -0300

    feat: implementa tela de catalogo com estados explicitos e documentação

--- mobile/lib/app/routes.dart
+++ mobile/lib/app/routes.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import '../../features/auth/presentation/pages/login_page.dart';
 import '../../features/auth/presentation/pages/welcome_page.dart';
 import '../../features/auth/presentation/pages/cadastro_page.dart';
+import '../../features/startups/presentation/pages/catalog_page.dart';
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
     '/welcome': (context) => const WelcomePage(),
     '/register': (context) => const CadastroPage(),
     '/login': (context) => const LoginPage(),
-    '/home': (context) => const Scaffold(body: Center(child: Text('Home'))),
+    '/home': (context) => const CatalogPage(),
   };
 }
