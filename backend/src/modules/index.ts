a5817d9e9703aa02e374cfffd5e0cceb623acfec
MiguelFMont <miguel.fermon10@gmail.com>
2026-04-30 11:12:55 -0300

    wip: tela de perfil + splash page

--- backend/src/modules/index.ts
+++ backend/src/modules/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import authRoutes from './auth/auth.routes';
 import startupsRoutes from './startups/startups.routes';
-// import usersRoutes from './users/users.routes';
+import usersRoutes from './users/users.routes';
 // import tokensRoutes from './tokens/tokens.routes';
 // import walletRoutes from './wallet/wallet.routes';
 // import questionsRoutes from './questions/questions.routes';
@@ -10,7 +10,7 @@ const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/startups', startupsRoutes);
-// router.use('/users', usersRoutes);
+router.use('/users', usersRoutes);
 // router.use('/tokens', tokensRoutes);
 // router.use('/wallet', walletRoutes);
 // router.use('/questions', questionsRoutes);
